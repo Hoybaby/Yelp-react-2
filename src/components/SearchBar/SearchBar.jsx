60c02d44144d8a9f184ef31a0d3df67057abc504
Hoybaby <mbartek436@gmail.com>
2021-05-04 17:09:04 -0400

    im building out the api interation with the searchbar so it can hit the paramaters fine.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,2 +1,19 @@
 import React from 'react';
 import './SearchBar.css';
+
+// need to create the ways for the search bar to search the api
+let sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated':'rating',
+    'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+    
+    // this function is to dynamically create list items needed to display the sort options. Should iterate through the keys and values 
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption]
+        })
+    }
+}
