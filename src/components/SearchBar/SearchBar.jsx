38fc6632c6ca48be5ea5e1f814079fffd359b820
Hoybaby <mbartek436@gmail.com>
2021-05-05 10:55:51 -0400

    made a function to handle the dynamic listing of the unorganized list. also added some JSX code to put some of the code oto the page with the css i already had

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,16 +4,39 @@ import './SearchBar.css';
 // need to create the ways for the search bar to search the api
 let sortByOptions = {
     'Best Match': 'best_match',
-    'Highest Rated':'rating',
+    'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
 }
 
 class SearchBar extends React.Component {
-    
+
     // this function is to dynamically create list items needed to display the sort options. Should iterate through the keys and values 
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption]
+            return <li key={sortByOptionValue}>{sortByOption}</li>
         })
     }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        )
+    }
+
 }
+
+export default SearchBar;
