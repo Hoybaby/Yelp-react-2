727d53f6f24aed190cdf7d8d9a58f451e725efb2
Hoybaby <mbartek436@gmail.com>
2021-05-11 00:05:51 -0400

    since the two new methods i created have a this.setState, i needed to add a bind to each in the constructor function which then was then placed on the input field

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -18,7 +18,7 @@ class SearchBar extends React.Component {
             term: '',
             location: '',
             sortBy: 'best_match'
-        }
+        };
         // currently user doesnt know what choice they are selected so this will help it.
 
         // this.sortByOptions = {
@@ -27,7 +27,8 @@ class SearchBar extends React.Component {
         //     'Most Reviewed': 'review_count'
         // }
 
-
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
 
     }
 
@@ -74,8 +75,8 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
