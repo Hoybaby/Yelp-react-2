7ffe6decdfe3944eff6ce5f00c6201ba6bc4690e
Hoybaby <mbartek436@gmail.com>
2021-05-17 12:07:56 -0400

    making the then responses for the search to be applied and returning the information i wish for it to be returned

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -8,6 +8,23 @@ const yelp = {
             },
         }).then((response) => {
             return response.json();
+        }).then((jsonResponse) => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+
+
+                    }
+                }));
+            }
         })
     }
-}
+};
