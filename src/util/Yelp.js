bdb11167bb260a3b6272536615c98381897d376e
Hoybaby <mbartek436@gmail.com>
2021-05-17 12:01:20 -0400

    making the interactions work with the yelp so i have to make a new file and pass the key and client id with it

---
+++ src/util/Yelp.js
@@ -0,0 +1,13 @@
+const apiKey = process.env(apiKey)
+
+const yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            },
+        }).then((response) => {
+            return response.json();
+        })
+    }
+}
